46e93c858865340d6b8ec6cf2cfbe2404062c5a4
Bac Nguyen Cong <nguyencongbacbk@gmail.com>
2019-02-12 15:29:35 +0100

    go

--- jobs.sh
+++ jobs.sh
@@ -13,9 +13,10 @@ IMG_SIZE=227  # image size
 BATCH_SIZE=128  # batch size
 EPOCHS=200      # number of epochs
 CHECK_POINT='./output/model_best.pth.tar'
+PRET='imagenet'
 
 # run train python script
-python train.py --data $DATA -a $ARCH -l $LOSS -img_size $IMG_SIZE -j 8 --lr $LR --epochs $EPOCHS --outdim $DIM -b $BATCH_SIZE --pretrained --seed 123456
+python train.py --data $DATA -a $ARCH -l $LOSS -img_size $IMG_SIZE -j 8 --lr $LR --epochs $EPOCHS --outdim $DIM -b $BATCH_SIZE --pretrained $PRET --seed 123456
 
 # run test python script
 python test.py --data $DATA -a $ARCH -c $CHECK_POINT -img_size $IMG_SIZE -j 8 --outdim $DIM -b $BATCH_SIZE --seed 123456
