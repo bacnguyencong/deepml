b985e195d7d969b66ec154d7cc157ffe6434bc76
Bac Nguyen Cong <nguyencongbacbk@gmail.com>
2019-02-12 17:30:05 +0100

    update

--- deepml/utils/libs.py
+++ deepml/utils/libs.py
@@ -221,6 +221,12 @@ def get_data_augmentation(img_size, mean, std, ttype):
         Transform: A transform.
     """
     # setup data augmentation
+    mean = np.array(mean)
+    std = np.array(std)
+    # fix the error
+    if (mean[0] > 1):
+        mean /= 255.0
+        std /= 255.0
     normalize = transforms.Normalize(mean=mean, std=std)
 
     if ttype == 'train':
--- train.py
+++ train.py
@@ -11,8 +11,6 @@ from deepml import datasets, losses
 from deepml.models import CNNs
 from deepml.utils import libs
 import pretrainedmodels
-from torchvision import transforms
-
 
 # list of data paths
 DATA_PATHS = {
@@ -139,8 +137,6 @@ if __name__ == "__main__":
                         help='mini-batch size (default: 256)')
     parser.add_argument('--lr', '--learning-rate', default=1e-5, type=float,
                         metavar='LR', help='initial learning rate', dest='lr')
-    parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
-                        help='momentum')
     parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
                         metavar='W', help='weight decay (default: 1e-4)',
                         dest='weight_decay')
