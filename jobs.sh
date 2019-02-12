81fb5ad2d08646d6cfe49deb93e958229a80953e
Bac Nguyen Cong <nguyencongbacbk@gmail.com>
2019-02-12 12:00:53 +0100

    add

--- jobs.sh
+++ jobs.sh
@@ -2,7 +2,6 @@
 source activate deepml-env
 
 export CUDA_VISIBLE_DEVICES=1
-logfile=$(date '+%d-%m-%Y-%H:%M:%S.txt');
 
 DATA='Cub'
 ARCH='bnincepnet'
@@ -16,7 +15,7 @@ EPOCHS=200      # number of epochs
 CHECK_POINT='./output/model_best.pth.tar'
 
 # run train python script
-python train.py --data $DATA -a $ARCH -l $LOSS -img_size $IMG_SIZE -j 8 --lr $LR --epochs $EPOCHS --outdim $DIM -b $BATCH_SIZE --pretrained --seed 123456 > ./output/train.log
+python train.py --data $DATA -a $ARCH -l $LOSS -img_size $IMG_SIZE -j 8 --lr $LR --epochs $EPOCHS --outdim $DIM -b $BATCH_SIZE --pretrained --seed 123456
 
 # run test python script
-python test.py --data $DATA -a $ARCH -c $CHECK_POINT -img_size $IMG_SIZE -j 8 --outdim $DIM -b $BATCH_SIZE --seed 123456 > ./output/test.log
+python test.py --data $DATA -a $ARCH -c $CHECK_POINT -img_size $IMG_SIZE -j 8 --outdim $DIM -b $BATCH_SIZE --seed 123456
