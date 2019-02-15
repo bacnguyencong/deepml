f89f3d57266f350a27b35fab18b66c285635427d
Bac Nguyen Cong <nguyencongbacbk@gmail.com>
2019-02-15 17:52:14 +0100

    fix

--- deepml/losses/jeffrey.py
+++ deepml/losses/jeffrey.py
@@ -7,6 +7,24 @@ class Jeffrey(nn.Module):
         super(Jeffrey, self).__init__()
 
     def forward(self, inputs, targets):
+        n = inputs.size(0)  # number of inputs
+        sim = list()  # total loss
+        dis = list()
+        for i in range(n):
+            dist = torch.pairwise_distance(inputs[i], inputs, keepdim=True)
+            dist = torch.pow(dist, 2)
+            # adding positive
+            pos = torch.masked_select(dist, targets == targets[i])
+            if pos.size(0) > 0:
+                sim.append(pos)
+            # adding negative
+            neg = torch.masked_select(dist, targets != targets[i])
+            if neg.size(0) > 0:
+                dis.append(neg)
+        a = torch.mean(torch.cat(sim, dim=0))
+        b = torch.mean(torch.cat(dis, dim=0))
+        return -a/b - b/a
+        """
         dist = torch.pairwise_distance(inputs, torch.cat(
             (inputs[-1:], inputs[:-1])), keepdim=True)
         dist = torch.pow(dist, 2)
@@ -24,3 +42,4 @@ class Jeffrey(nn.Module):
         else:
             print('no neg found')
         return -pos / neg - neg / pos
+        """
