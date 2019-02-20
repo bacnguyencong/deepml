f7557d8622f64fb031b7d933c1f17f56a8a89f36
Bac Nguyen Cong <nguyencongbacbk@gmail.com>
2019-02-21 00:02:40 +0100

    go

--- jobs.sh
+++ jobs.sh
@@ -10,11 +10,11 @@ LOSS='SymTripLoss'
 LR=0.0001
 DIM=128         # embedded feature size
 IMG_SIZE=224    # image size
-BATCH_SIZE=128  # batch size
+BATCH_SIZE=150  # batch size
 EPOCHS=200      # number of epochs
 CHECK_POINT='./output/model_best.pth.tar'
 PRET='imagenet'
-TAR=3
+TAR=5
 
 # run train python script
 python train.py --data $DATA -a $ARCH -l $LOSS -n_targets $TAR -img_size $IMG_SIZE -j 8 --lr $LR --epochs $EPOCHS --outdim $DIM -b $BATCH_SIZE --pretrained $PRET --seed 123456  --normalized
