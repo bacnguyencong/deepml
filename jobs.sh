2e4d2a3e43e509e9f33c70f8ef21bd27d2add2d3
Bac Nguyen Cong <nguyencongbacbk@gmail.com>
2019-02-23 16:37:33 +0100

    fixed

--- jobs.sh
+++ jobs.sh
@@ -7,14 +7,14 @@ DATA='Cub'
 ARCH='bninception'
 LOSS='SymTripLoss'
 
-LR=0.0001
+LR=0.001
 DIM=128         # embedded feature size
 IMG_SIZE=224    # image size
 BATCH_SIZE=128  # batch size
 EPOCHS=200      # number of epochs
 CHECK_POINT='./output/model_best.pth.tar'
 PRET='imagenet'
-TAR=7
+TAR=5
 
 # run train python script
 python train.py --data $DATA -a $ARCH -l $LOSS -n_targets $TAR -img_size $IMG_SIZE -j 8 --lr $LR --epochs $EPOCHS --outdim $DIM -b $BATCH_SIZE --pretrained $PRET --seed 123456  
